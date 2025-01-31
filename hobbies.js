645ae7eb53b87fcbc6a39034f10409dc7ac927ba
nicolamarino <nicolamarino527@gmail.com>
2025-01-31 18:30:59 +0100

    [feat] create object function and exported

---
+++ hobbies.js
@@ -0,0 +1,4 @@
+// 2 - hobbies.js
+// 1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree.
+//  La funzione dovrebbe restituire un array di stringhe contenenti gli hobby.
+// 2. Esporta la funzione dal file.
---
+++ peoples.js
@@ -0,0 +1,6 @@
+// 3 - people.js
+// 1. Importa la tua funzione da names.js
+// 2. Importa la tua funzione da hobbies.js
+// 3. Crea una funzione che non ha parametri. La funzione dovrebbe restituire 
+// un oggetto con due proprietà: fullName ed hobbies. All’interno della tua funzione,
+//  usa le tue due funzioni precedenti per costruire l’oggetto.
