f823860b4e616f1ad8a2e85bdc803cd4c7256380
nicolamarino <nicolamarino527@gmail.com>
2025-01-31 19:04:34 +0100

    [feat] add hobbies and exported

--- hobbies.js
+++ hobbies.js
@@ -2,3 +2,25 @@
 // 1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree.
 //  La funzione dovrebbe restituire un array di stringhe contenenti gli hobby.
 // 2. Esporta la funzione dal file.
+
+
+//  aggiungiiamo la funzione che ci ritorna l oggetto degli hobbies
+function addHobbies(hobbyOne, hobbyTwo, hobbyThree) {
+    return {
+        hobby1 : hobbyOne,
+        hobby2 : hobbyTwo,
+        hobby3 : hobbyThree,
+    }
+};
+
+// Scriviamo la costante degli hobbies
+const hobbies = addHobbies('calcio','palestra','guardare le foglie che cadono');
+
+// Mostriamo gli hobbies a schermo
+console.log(hobbies);
+
+// Esportismo la funzione
+
+module.exports = {
+    addHobbies
+};
--- peoples.js
+++ peoples.js
@@ -4,3 +4,7 @@
 // 3. Crea una funzione che non ha parametri. La funzione dovrebbe restituire 
 // un oggetto con due proprietà: fullName ed hobbies. All’interno della tua funzione,
 //  usa le tue due funzioni precedenti per costruire l’oggetto.
+
+
+
+
